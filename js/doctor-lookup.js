a81700a3d92c21ceea43a94a1fc1f3a237e0f48e
Senya Webster <senya.webster@gmail.com>
2017-10-22 09:17:20 -0700

    set up, and get started

---
+++ js/doctor-lookup.js
@@ -0,0 +1,32 @@
+export class Lookup {
+
+  FindDocByIssue() {
+
+
+
+
+    // Return : Doctor first name, last name, address, phone number, website, accepting new patients (y/n)
+  }
+
+  FindDocByName() {
+
+
+    // Return : Doctor first name, last name, address, phone number, website, accepting new patients (y/n)
+  }
+
+
+}
+
+
+
+
+
+// A user should be able to enter a medical issue to receive a list of doctors in the Portland area that fit the search query.
+
+// A user should be able to to enter a name to receive a list of doctors in the Portland area that fit the search query.
+
+// If the query response includes any doctors, the following information should be included about each doctor: first name, last name, address, phone number, website and whether or not the doctor is accepting new patients (the API provides this data).
+
+// If the API call results in an error (any message not a 200 OK), the application should return a notification that states what the error is.
+
+// If the query response doesn't include any doctors (for instance, if no doctors meet the search criteria), the application should return a notification that states that no doctors meet the criteria. (This is not an error so it should be handled separately from any errors.)
